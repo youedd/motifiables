d692b0f2304c2bb3949cac6a884b652861cc5504
youssef eddibili <eddibili.youssef@gmail.com>
2022-04-25 20:18:59 +0200

    Custom definitions Example

---
+++ Examples/CustomDefinitions/App.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { MotiImage } from 'moti'
+import { Button, SafeAreaView, StyleSheet } from 'react-native'
+import { Motifiable, useDynamicMotifiable } from '../../src'
+import './definitions'
+
+const App: React.FC = () => {
+  const { animateTo: animateDoggo, props } = useDynamicMotifiable()
+
+  console.log(JSON.stringify(props, null, 2))
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Motifiable name='fadeInUpBig' duration={800}>
+        <MotiImage
+          {...props}
+          style={styles.doggo}
+          source={require('./doggo.png')}
+        />
+
+        <Button
+          title='new rotate'
+          onPress={() => animateDoggo({
+            name: 'rotate'
+          })}
+        />
+        <Button
+          title='custom'
+          onPress={() => animateDoggo({
+            name: 'custom'
+          })}
+        />
+      </Motifiable>
+
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  doggo: {
+    alignSelf: 'center',
+    width: 200,
+    height: 200
+  }
+})
+
+export default App
---
+++ Examples/CustomDefinitions/definitions.ts
@@ -0,0 +1,45 @@
+import { Easing } from 'react-native-reanimated'
+import { MotifiableDefinition, setMotifiableDefinition } from '../../src'
+
+// override definition
+setMotifiableDefinition('rotate', {
+  0: {
+    rotate: '0deg'
+  },
+
+  1: {
+    rotate: '360deg'
+  }
+})
+
+// add custom definition
+setMotifiableDefinition('custom', {
+  easing: Easing.linear,
+  0: {
+    rotate: '0deg',
+    opacity: 1
+  },
+  0.25: {
+    opacity: 0
+  },
+  0.5: {
+    opacity: 1,
+    rotate: '180deg'
+  },
+  0.75: {
+    opacity: 0
+  },
+  1: {
+    rotate: '360deg',
+    opacity: 1
+  }
+})
+
+/* eslint-disable */
+declare global {
+  namespace Motifiables {
+    interface MotifiableDefinitions {
+      custom: MotifiableDefinition
+    }
+  }
+}

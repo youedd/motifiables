a94a5495266a431c3483ff0268b818db957f46a3
youssef eddibili <eddibili.youssef@gmail.com>
2022-04-25 20:18:27 +0200

    add motifiables namespace

--- src/definitions/types.ts
+++ src/definitions/types.ts
@@ -11,90 +11,96 @@ export type MotifiableDefinition = Partial<Record<KeyframePosition, StyleValues>
   easing: Animated.EasingFunction
 }>
 
-export type MotifiableKey = keyof Motifaibles
+export type MotifiableKey = keyof Motifiables.MotifiableDefinitions
 
-export interface Motifaibles {
-  // attention-seekers
-  bounce: MotifiableDefinition
-  flash: MotifiableDefinition
-  jello: MotifiableDefinition
-  pulse: MotifiableDefinition
-  rotate: MotifiableDefinition
-  shake: MotifiableDefinition
-  swing: MotifiableDefinition
-  rubberBand: MotifiableDefinition
-  tada: MotifiableDefinition
-  wobble: MotifiableDefinition
+/*eslint-disable*/
+declare global {
+  namespace Motifiables {
+   export interface MotifiableDefinitions {
+      // attention-seekers
+      bounce: MotifiableDefinition
+      flash: MotifiableDefinition
+      jello: MotifiableDefinition
+      pulse: MotifiableDefinition
+      rotate: MotifiableDefinition
+      shake: MotifiableDefinition
+      swing: MotifiableDefinition
+      rubberBand: MotifiableDefinition
+      tada: MotifiableDefinition
+      wobble: MotifiableDefinition
 
-  // bouncing-entrances
-  bounceIn: MotifiableDefinition
-  bounceInUp: MotifiableDefinition
-  bounceInDown: MotifiableDefinition
-  bounceInRight: MotifiableDefinition
-  bounceInLeft: MotifiableDefinition
+      // bouncing-entrances
+      bounceIn: MotifiableDefinition
+      bounceInUp: MotifiableDefinition
+      bounceInDown: MotifiableDefinition
+      bounceInRight: MotifiableDefinition
+      bounceInLeft: MotifiableDefinition
 
-  // bouncing-exits
-  bounceOut: MotifiableDefinition
-  bounceOutUp: MotifiableDefinition
-  bounceOutDown: MotifiableDefinition
-  bounceOutRight: MotifiableDefinition
-  bounceOutLeft: MotifiableDefinition
+      // bouncing-exits
+      bounceOut: MotifiableDefinition
+      bounceOutUp: MotifiableDefinition
+      bounceOutDown: MotifiableDefinition
+      bounceOutRight: MotifiableDefinition
+      bounceOutLeft: MotifiableDefinition
 
-  // fading-entrances
-  fadeIn: MotifiableDefinition
-  fadeInDown: MotifiableDefinition
-  fadeInUp: MotifiableDefinition
-  fadeInLeft: MotifiableDefinition
-  fadeInRight: MotifiableDefinition
-  fadeInDownBig: MotifiableDefinition
-  fadeInUpBig: MotifiableDefinition
-  fadeInLeftBig: MotifiableDefinition
-  fadeInRightBig: MotifiableDefinition
+      // fading-entrances
+      fadeIn: MotifiableDefinition
+      fadeInDown: MotifiableDefinition
+      fadeInUp: MotifiableDefinition
+      fadeInLeft: MotifiableDefinition
+      fadeInRight: MotifiableDefinition
+      fadeInDownBig: MotifiableDefinition
+      fadeInUpBig: MotifiableDefinition
+      fadeInLeftBig: MotifiableDefinition
+      fadeInRightBig: MotifiableDefinition
 
-  // fading-exits
-  fadeOut: MotifiableDefinition
-  fadeOutDown: MotifiableDefinition
-  fadeOutUp: MotifiableDefinition
-  fadeOutLeft: MotifiableDefinition
-  fadeOutRight: MotifiableDefinition
-  fadeOutDownBig: MotifiableDefinition
-  fadeOutUpBig: MotifiableDefinition
-  fadeOutLeftBig: MotifiableDefinition
-  fadeOutRightBig: MotifiableDefinition
+      // fading-exits
+      fadeOut: MotifiableDefinition
+      fadeOutDown: MotifiableDefinition
+      fadeOutUp: MotifiableDefinition
+      fadeOutLeft: MotifiableDefinition
+      fadeOutRight: MotifiableDefinition
+      fadeOutDownBig: MotifiableDefinition
+      fadeOutUpBig: MotifiableDefinition
+      fadeOutLeftBig: MotifiableDefinition
+      fadeOutRightBig: MotifiableDefinition
 
-  // flippers
-  // flipInX: MotifiableDefinition
-  // flipInY: MotifiableDefinition
-  // flipOutX: MotifiableDefinition
-  // flipOutY: MotifiableDefinition
+      // flippers
+      // flipInX: MotifiableDefinition
+      // flipInY: MotifiableDefinition
+      // flipOutX: MotifiableDefinition
+      // flipOutY: MotifiableDefinition
 
-  // lightspeed
-  lightSpeedIn: MotifiableDefinition
-  lightSpeedOut: MotifiableDefinition
+      // lightspeed
+      lightSpeedIn: MotifiableDefinition
+      lightSpeedOut: MotifiableDefinition
 
-  // sliding-entrances
-  slideInDown: MotifiableDefinition
-  slideInUp: MotifiableDefinition
-  slideInLeft: MotifiableDefinition
-  slideInRight: MotifiableDefinition
+      // sliding-entrances
+      slideInDown: MotifiableDefinition
+      slideInUp: MotifiableDefinition
+      slideInLeft: MotifiableDefinition
+      slideInRight: MotifiableDefinition
 
-  // sliding-exits
-  slideOutDown: MotifiableDefinition
-  slideOutUp: MotifiableDefinition
-  slideOutLeft: MotifiableDefinition
-  slideOutRight: MotifiableDefinition
+      // sliding-exits
+      slideOutDown: MotifiableDefinition
+      slideOutUp: MotifiableDefinition
+      slideOutLeft: MotifiableDefinition
+      slideOutRight: MotifiableDefinition
 
-  // zooming-entrances
-  zoomIn: MotifiableDefinition
-  zoomInDown: MotifiableDefinition
-  zoomInUp: MotifiableDefinition
-  zoomInLeft: MotifiableDefinition
-  zoomInRight: MotifiableDefinition
+      // zooming-entrances
+      zoomIn: MotifiableDefinition
+      zoomInDown: MotifiableDefinition
+      zoomInUp: MotifiableDefinition
+      zoomInLeft: MotifiableDefinition
+      zoomInRight: MotifiableDefinition
 
-  // zooming-exits
-  zoomOut: MotifiableDefinition
-  zoomOutDown: MotifiableDefinition
-  zoomOutUp: MotifiableDefinition
-  zoomOutLeft: MotifiableDefinition
-  zoomOutRight: MotifiableDefinition
+      // zooming-exits
+      zoomOut: MotifiableDefinition
+      zoomOutDown: MotifiableDefinition
+      zoomOutUp: MotifiableDefinition
+      zoomOutLeft: MotifiableDefinition
+      zoomOutRight: MotifiableDefinition
+    }
+
+  }
 }
